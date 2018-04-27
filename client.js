8d47930fee0e82e03ec1de6a8ddcd658169abe54
ivc13020adm <stash_admin@bitbucket.org>
2018-04-26 20:13:54 -0500

    first commit

---
+++ client.js
@@ -0,0 +1,139 @@
+var getUserInput = require('readline-sync');
+var chalk = require('chalk');
+
+
+var thisClient = this;
+
+this.ipAddr = process.argv[2] || '127.0.0.1';
+this.portNo = process.argv[3] || 5050;
+this.ipUrl = 'http://' + this.ipAddr + ':' + this.portNo;
+this.clientSocket = null;
+this.clientId = null;
+this.gameBoard = [9];
+
+var connectToServer = function (thisClient) {
+    thisClient.clientSocket = require('socket.io-client')(thisClient.ipUrl);
+
+    thisClient.clientSocket.on('connect', function (socket) {
+        console.log('Connected to ' + thisClient.ipAddr + ' ' + thisClient.portNo);
+    }.bind(thisClient));
+
+    thisClient.clientSocket.on('init', function (msg, clientId) {
+        console.log(msg);
+        thisClient.clientId = clientId;
+        //console.log('Your clientId is '+ thisClient.clientId);
+    }.bind(thisClient));
+
+    thisClient.clientSocket.on('gameBegins', function (message) {
+        var position = '';
+        if (thisClient.clientId == 0) {
+            position = 'first';
+        }
+        else {
+            position = 'second';
+        }
+        var msg = 'Game Started. You are the ' + position + ' player.';
+
+        console.log(msg);
+        //console.log(message.gameBoard);
+        thisClient.gameBoard = message.gameBoard;
+        //console.log(clientId);
+        thisClient.drawBoard();
+        thisClient.whoseMove(message.currentPlayer);
+    }.bind(thisClient));
+
+    thisClient.clientSocket.on('gameMove', function (message) {
+
+        thisClient.gameBoard = message.gameBoard;
+        thisClient.drawBoard();
+        thisClient.whoseMove(message.currentPlayer);
+
+    });
+
+//gameResigned
+
+    thisClient.clientSocket.on('gameResigned', function (message) {
+        //console.log(message);
+        //thisClient.gameBoard = message.gameBoard;
+        thisClient.showGameStatus(message);
+        thisClient.clientSocket.emit('disconnect');
+    }.bind(thisClient));
+
+    thisClient.clientSocket.on('gameOver', function (message) {
+        //console.log(message);
+        thisClient.gameBoard = message.gameBoard;
+        thisClient.drawBoard();
+        thisClient.showGameStatus(message);
+        thisClient.clientSocket.emit('disconnect');
+    }.bind(thisClient));
+
+
+};
+
+this.whoseMove = function (currentPlayer) {
+    if (currentPlayer == thisClient.clientId) {
+        //console.log('starting game');
+        console.log(chalk.blue('Its your move... (Type 1-9 or "r" to resign)...'));
+        var validInput = false;
+        while (validInput === false) {
+            var userInput = getUserInput.question('>');
+            //console.log('userInput '+userInput);
+
+            if (userInput === 'r' || userInput > 0 && userInput <=9) {
+                validInput = true;
+            }
+            else {
+                console.log(chalk.blue('Invalid input. (Type 1-9 or "r" to resign)...'));
+            }
+        }
+        thisClient.clientSocket.emit('userMove', thisClient.clientId, userInput);
+    }
+    else {
+        console.log(chalk.blue('Wait for opponent to make a move...'));
+    }
+};
+
+this.drawBoard = function () {
+/*
+    console.log('in drawboard');
+    console.log(thisClient.gameBoard);
+    console.log(thisClient.gameBoard.length);
+*/
+
+
+    var row1 = thisClient.gameBoard[0] + ' ' + thisClient.gameBoard[1] + ' ' + thisClient.gameBoard[2];
+    var row2 = thisClient.gameBoard[3] + ' ' + thisClient.gameBoard[4] + ' ' + thisClient.gameBoard[5];
+    var row3 = thisClient.gameBoard[6] + ' ' + thisClient.gameBoard[7] + ' ' + thisClient.gameBoard[8];
+
+    console.log(chalk.green('------------------------- Current Board -------------------------'));
+    console.log(chalk.green(row1));
+    console.log(chalk.green(row2));
+    console.log(chalk.green(row3));
+};
+
+this.showGameStatus = function (message) {
+    if (message.status == 'Win') {
+        if (message.player == thisClient.clientId) {
+            console.log(chalk.green('You win!!!'));
+        }
+        else {
+            console.log(chalk.red('Sorry. You Lost!!!'));
+        }
+
+    }
+    else if (message.status == 'Resign') {
+        if (message.player == thisClient.clientId) {
+            console.log(chalk.green('You resigned'));
+        }
+        else {
+            console.log(chalk.red('Sorry. Opponent has resigned the game'));
+        }
+
+    }
+    else if (message.status == 'Tie') {
+        console.log(chalk.red('Game Tied'));
+    }
+
+};
+
+connectToServer(thisClient);
---
+++ server.js
@@ -0,0 +1,147 @@
+'use strict';
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var thisServer = this;
+
+this.clients = [];
+this.portNumber = process.argv[2] || 5050;
+this.clientCount = 0;
+this.gameBoard = ['.', '.', '.', '.', '.', '.', '.', '.', '.'];
+this.currentPlayer = 0;
+this.gameStatus = 0;
+this.gameMoveCount = 0;
+
+
+http.listen(this.portNumber, function(){
+    console.log('listening on ' + thisServer.portNumber);
+}.bind(thisServer));
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    //console.log(socket);
+    var newClientId = thisServer.clientCount++;
+    thisServer.clients[newClientId] = socket;
+    thisServer.communicate(newClientId, 'init', 'Welcome to Tic-Tac-Toe');
+
+    if (this.clients.length === 2) {
+        console.log('Two clients joined');
+        //console.log(thisServer.gameBoard);
+        thisServer.gameStatus = 1;
+        thisServer.broadcast('gameBegins', {
+            gameBoard: thisServer.gameBoard,
+            currentPlayer: thisServer.currentPlayer
+        });
+    }
+
+    socket.on('disconnect', function (message) {
+        console.log('Client disconnected')
+    });
+
+    socket.on('userMove', function (clientId, userInput) {
+        //console.log(userInput);
+        //console.log(clientId);
+        thisServer.gameMoveCount++;
+
+        if (userInput === 'r') {
+            thisServer.broadcast('gameResigned', {
+                status: 'Resign',
+                player: clientId
+            });
+        }
+        else {
+            if (clientId === 0) {
+                thisServer.gameBoard[userInput-1] = 'X'
+            }
+            else {
+                thisServer.gameBoard[userInput-1] = 'O'
+            }
+            if (thisServer.checkWins(clientId)) {
+                console.log('Game Over');
+                thisServer.gameStatus = 2;
+                thisServer.broadcast('gameOver', {
+                    status: 'Win',
+                    player: clientId,
+                    gameBoard: thisServer.gameBoard
+                });
+            }
+
+            if (thisServer.currentPlayer == 1) {
+                thisServer.currentPlayer = 0;
+            }
+            else if (thisServer.currentPlayer == 0) {
+                thisServer.currentPlayer = 1;
+            }
+
+            //console.log(thisServer.gameBoard);
+            //console.log('thisServer.currentPlayer');
+            //console.log(thisServer.currentPlayer);
+            //console.log(!thisServer.currentPlayer);
+            if (thisServer.gameStatus === 1) {
+                thisServer.broadcast('gameMove', {
+                    gameBoard: thisServer.gameBoard,
+                    currentPlayer: thisServer.currentPlayer
+                });
+            }
+
+            if (thisServer.gameMoveCount === 9) {
+                thisServer.broadcast('gameOver', {
+                    status: 'Tie',
+                    gameBoard: thisServer.gameBoard
+                });
+            }
+        }
+
+
+    });
+
+
+
+}.bind(thisServer));
+
+
+this.communicate = function (clientId, channel, message) {
+    this.clients[clientId].emit(channel, message, clientId);
+
+};
+
+this.broadcast = function (channel, message) {
+    for (const clientId in this.clients) {
+        this.communicate(clientId, channel, message);
+    }
+};
+
+this.startGame = function () {
+    this.gameBoard = [9];
+
+
+};
+
+this.checkWins = function (clientId) {
+    var clientSym = '';
+    if (clientId === 0) {
+        clientSym = 'X'
+    }
+    else {
+        clientSym = 'O'
+    }
+
+    return this.checkPosition(clientSym, 1, 2, 3)
+    || this.checkPosition(clientSym, 4, 5, 6)
+        || this.checkPosition(clientSym, 7, 8, 9)
+        || this.checkPosition(clientSym, 1, 4, 7)
+        || this.checkPosition(clientSym, 2, 5, 8)
+        || this.checkPosition(clientSym, 3, 6, 9)
+        || this.checkPosition(clientSym, 1, 5, 9)
+        || this.checkPosition(clientSym, 3, 5, 7)
+
+};
+
+this.checkPosition = function (clientSym, pos1, pos2, pos3) {
+    if (this.gameBoard[pos1-1] === clientSym && this.gameBoard[pos2-1] === clientSym  && this.gameBoard[pos3-1] === clientSym ) {
+        return true;
+    }
+};
+
